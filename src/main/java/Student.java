faae9bcfdbf3a16d6539cd26841faba27949698c
iasmin <icb2@ic.ufal.br>
2024-05-13 11:48:26 -0300

    Criando o construtor Student com um único parâmetro nome do tipo sting e criando a variável local student

---
+++ src/main/java/Student.java
@@ -0,0 +1,5 @@
+class Student {
+    Student(String name){
+
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -2,5 +2,6 @@ import junit.framework.TestCase;
 
 public class StudentTest extends TestCase {
     public void testCreate() {
+        Student student = new Student("Jane Doe");
     }
 }
