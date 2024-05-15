a6503bb12e5872ea07c755440364175c87a40f60
iasmin <icb2@ic.ufal.br>
2024-05-15 09:02:20 -0300

    Adiciona uma coleção de testes, uma classe "CourseSession" e sua classe de teste.

---
+++ src/main/java/CourseSession.java
@@ -0,0 +1,31 @@
+class CourseSession {
+    private String department;
+    private String number;
+    private int numberOfStudents;
+    private java.util.ArrayList<Student> students = new java.util.ArrayList<Student>();
+
+    CourseSession(String department, String number){
+        this.department = department;
+        this.number = number;
+        numberOfStudents = 0;
+    }
+
+    String getDepartment(){
+        return department;
+    }
+    String getNumber(){
+        return number;
+    }
+    int getNumberOfStudents(){
+        return  numberOfStudents;
+    }
+
+    void enroll(Student student){
+        numberOfStudents = numberOfStudents+1;
+        students.add(student);
+    }
+
+    java.util.ArrayList<Student> getAllStudents(){
+        return students;
+    }
+}
