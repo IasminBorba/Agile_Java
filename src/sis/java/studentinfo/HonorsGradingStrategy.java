5f147110b93e50c8b5dca36368eba26b7523080a
iasmin <icb2@ic.ufal.br>
2024-06-04 08:07:54 -0300

    Cria a superclasse BasicGradingStrategy e a classe ReportCard. Modifica as classes HonorsGradingStrategy e RegularGradingStrategy para herdar da superclasse.

--- src/sis/java/studentinfo/HonorsGradingStrategy.java
+++ src/sis/java/studentinfo/HonorsGradingStrategy.java
@@ -1,20 +1,11 @@
 package studentinfo;
 
 
-public class HonorsGradingStrategy implements GradingStrategy {
+public class HonorsGradingStrategy extends BasicGradingStrategy {
     public int getGradePointsFor(Student.Grade grade) {
-        int points = basicGradePointsFor(grade);
+        int points = super.getGradePointsFor(grade);
         if (points > 0)
             points += 1;
         return points;
-    }
-    int basicGradePointsFor(Student.Grade grade) {
-        return switch (grade) {
-            case A -> 4;
-            case B -> 3;
-            case C -> 2;
-            case D -> 1;
-            default -> 0;
-        };
     }
 }
