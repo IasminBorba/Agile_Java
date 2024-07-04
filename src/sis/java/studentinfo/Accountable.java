9c51b2cf1fbedc8f378c79b9eaa837b1f301e1c2
iasmin <icb2@ic.ufal.br>
2024-07-04 13:15:19 -0300

    Cria as classes AccountFactory, Permission e SecureProxy, além das suas classes de testes. Além disso, cria a interface Accountable e move as classes de testes de sis para test.

---
+++ src/sis/java/studentinfo/Accountable.java
@@ -0,0 +1,13 @@
+package studentinfo;
+
+import java.math.*;
+
+public interface Accountable {
+    void credit(BigDecimal amount);
+    BigDecimal getBalance();
+    BigDecimal transactionAverage();
+    void setBankAba(String bankAba);
+    void setBankAccountNumber(String bankAccountNumber);
+    void setBankAccountType(Account.BankAccountType bankAccountType);
+    void transferFromBank(BigDecimal amount);
+}
---
+++ src/test/java/security/SecureProxyTest.java
@@ -0,0 +1,42 @@
+package security;
+
+import junit.framework.*;
+import java.lang.reflect.Method;
+
+public class SecureProxyTest extends TestCase {
+    private static final String secureMethodName = "secure";
+    private static final String insecureMethodName = "insecure";
+    private Object object;
+    private SecureProxy proxy;
+    private boolean secureMethodCalled;
+    private boolean insecureMethodCalled;
+
+    protected void setUp(){
+        object = new Object() {
+            public void secure() {
+                secureMethodCalled = true;
+            }
+            public void insecure() {
+                insecureMethodCalled = true;
+            }
+        };
+        proxy = new SecureProxy(object, secureMethodName);
+    }
+
+    public void testSecureMethod() throws Throwable {
+        Method secureMethod = object.getClass().getDeclaredMethod(secureMethodName);
+        try {
+            proxy.invoke(proxy, secureMethod, new Object[]{});
+            fail("expected PermissionException");
+        }
+        catch (PermissionException expected) {
+            assertFalse(secureMethodCalled);
+        }
+    }
+
+    public void testInsecureMethod() throws Throwable {
+        Method insecureMethod = object.getClass().getDeclaredMethod(insecureMethodName);
+        proxy.invoke(proxy, insecureMethod, new Object[]{});
+        assertTrue(insecureMethodCalled);
+    }
+}
