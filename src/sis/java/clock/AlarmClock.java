1485073f8a95c22d7e283559ee6b63999bbe1689
iasmin <icb2@ic.ufal.br>
2024-07-19 11:54:40 -0300

    Refatoranco a classe AlarmClock e sua classe de teste.

--- src/sis/java/clock/AlarmClock.java
+++ src/sis/java/clock/AlarmClock.java
@@ -6,10 +6,15 @@ import java.util.*;
 public class AlarmClock implements Runnable{
     private final ClockListener alarm;
     private boolean run = true;
+    public static List<List<Object>> alarms;
+    private static final Object monitor = new Object();
+    private static final StringBuilder textAlarms = new StringBuilder();
 
-    public AlarmClock(ClockListener alarm){
-        this.alarm = alarm;
+    public AlarmClock(List<List<Object>> alarms) throws InterruptedException {
+        this.alarms = alarms;
+        this.alarm = createClockListener();
         new Thread(this).start();
+        timer();
     }
 
     public void stop(){
@@ -29,5 +34,100 @@ public class AlarmClock implements Runnable{
                 }
             }
         }
+    }
+
+    private void timer() throws InterruptedException {
+        synchronized (monitor){
+            monitor.wait();
+        }
+        this.stop();
+    }
+
+    private static ClockListener createClockListener() {
+        title();
+        return new ClockListener() {
+            int count = 0;
+            public void update(Date date){
+                addText(date);
+
+                int hourNow = date.getHours();
+                int minuteNow = date.getMinutes();
+                for(List<Object> list: alarms) {
+                    if (hourNow == (Integer) list.getFirst() && minuteNow == (Integer) list.get(1)){
+                        addText(list.getLast());
+                        count++;
+                    }
+                }
+
+                if (count == alarms.size()) {
+                    synchronized (monitor) {
+                        monitor.notifyAll();
+                    }
+                }
+            }
+        };
+    }
+
+    static List<Object> convertHourText(String time, String text) {
+        int hour = 0;
+        int minute = 0;
+        int count = 0;
+
+        for(String str: time.split(":")) {
+            if(++count == 1)
+                hour = Integer.parseInt(str);
+            else
+                minute = Integer.parseInt(str);
+        }
+        return List.of(hour, minute, text);
+    }
+
+    private static void title(){
+        StringBuilder builder = new StringBuilder("Alarm List: ");
+
+        sortAlarmList();
+        for(List<Object> list: alarms){
+            int hour = (Integer)list.getFirst();
+            String hourString = (hour < 10) ? "0" + hour : Integer.toString(hour);
+            int minute = (Integer)list.get(1);
+            String minuteString = (minute < 10) ? "0" + minute : Integer.toString(minute);
+
+            builder.append(hourString).append(":").append(minuteString).append("h, ");
+        }
+        builder.delete(builder.length() - 2, builder.length());
+        System.out.println(builder.append("\n"));
+    }
+
+    public static void sortAlarmList(){
+        alarms.sort(new Comparator<>() {
+            @Override
+            public int compare(List<Object> alarm1, List<Object> alarm2) {
+                int hour1 = (Integer) alarm1.get(0);
+                int minute1 = (Integer) alarm1.get(1);
+                int hour2 = (Integer) alarm2.get(0);
+                int minute2 = (Integer) alarm2.get(1);
+
+                int hourCompare = Integer.compare(hour1, hour2);
+                if (hourCompare != 0) {
+                    return hourCompare;
+                }
+
+                return Integer.compare(minute1, minute2);
+            }
+        });
+    }
+
+    private static void addText(Object obj) {
+        if (obj.getClass().toString().equals("class java.util.Date")){
+            textAlarms.append(obj).append("\n");
+            System.out.println(obj);
+        } else {
+            textAlarms.append("ALARM: ").append(obj).append("\n");
+            System.out.println("ALARM: " + obj + "\n");
+        }
+    }
+
+    public String printAlarms(){
+        return textAlarms.toString();
     }
 }
--- src/test/java/clock/AlarmClockTest.java
+++ src/test/java/clock/AlarmClockTest.java
@@ -1,54 +1,58 @@
 package clock;
 
 import junit.framework.TestCase;
-import java.time.LocalDateTime;
 import java.util.*;
+
+import static clock.AlarmClock.convertHourText;
 
 public class AlarmClockTest extends TestCase {
     private AlarmClock clock;
-    private final Object monitor = new Object();
-//ClockListener listener;
+    public static List<List<Object>> alarms;
+
     public void testAlarm() throws InterruptedException {
-        int hour = 11;
-        int minute = 49;
-        ArrayList<List<Integer>> alarms = new ArrayList<>();
-//        for(int i = 1; i < 3; i++)
-//            alarms.add(List.of(hour, minute+i));
-         alarms.add(List.of(hour, minute+1));
-         alarms.add(List.of(hour, minute+3));
-        System.out.println("Alarm List:  " + alarms + "\n");
+        alarms = new ArrayList<>();
+        String alarm = "11:54";
+        String textAlarm = "Wake up!";
+        createAlarm(alarm, textAlarm);
 
-        String textAlarm = "Wake up!\n";
-        ClockListener listener = createClockListener(alarms, textAlarm);
-        clock = new AlarmClock(listener);
-        synchronized (monitor){
-            monitor.wait();
-        }
-        clock.stop();
-//        verifyAlarm(alarms, listener);
+        clock = new AlarmClock(alarms);
+
+        String textAlarms =
+                "Fri Jul 19 11:53:00 BRT 2024\n" +
+                "Fri Jul 19 11:54:00 BRT 2024\n" +
+                "ALARM: Wake up!\n";
+        assertEquals(textAlarms, clock.printAlarms());
     }
 
-    private ClockListener createClockListener(ArrayList<List<Integer>> alarms, String textAlarm) {
-        return new ClockListener() {
-            int count = 1;
-            public void update(Date date){
-                System.out.println(date);
-                if (alarms.contains(List.of(date.getHours(), date.getMinutes()))){
-                    System.out.println(textAlarm);
-                } else if (count == alarms.size()) {
-                    synchronized (monitor) {
-                        monitor.notifyAll();
-                    }
-                }
-                count++;
-            }
-        };
+    public void testAlarms() throws InterruptedException {
+        alarms = new ArrayList<>();
+
+        String alarm1 = "10:55";
+        String textAlarm1 = "You are late!";
+        createAlarm(alarm1, textAlarm1);
+
+        String alarm2 = "10:52";
+        String textAlarm2 = "Wake up!";
+        createAlarm(alarm2, textAlarm2);
+
+        String alarm3 = "10:53";
+        String textAlarm3 = "Now!!";
+        createAlarm(alarm3, textAlarm3);
+
+        clock = new AlarmClock(alarms);
+
+        String textAlarms =
+                "Fri Jul 19 10:52:00 BRT 2024\n" +
+                        "ALARM: Wake up!\n" +
+                        "Fri Jul 19 10:53:00 BRT 2024\n" +
+                        "ALARM: Now!!\n" +
+                        "Fri Jul 19 10:54:00 BRT 2024\n" +
+                        "Fri Jul 19 10:55:00 BRT 2024\n" +
+                        "ALARM: You are late!\n";
+        assertEquals(textAlarms, clock.printAlarms());
     }
 
-    private void verifyAlarm(int hour, int minute){
-        LocalDateTime now = LocalDateTime.now();
-        assertEquals(hour, now.getHour());
-        assertEquals(minute, now.getMinute());
-        //for(alarm: alarms)
+    public void createAlarm(String alarm, String textAlarm){
+        alarms.add(convertHourText(alarm, textAlarm));
     }
 }
