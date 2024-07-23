dab531476120da543dc27bba2aca9697cedebacf
iasmin <icb2@ic.ufal.br>
2024-07-23 13:38:02 -0300

    Cria a classe de teste TestsTest

---
+++ src/test/java/testing/TestsTest.java
@@ -0,0 +1,18 @@
+package testing;
+
+import junit.framework.TestCase;
+import java.util.*;
+
+public class TestsTest extends TestCase {
+    public void testArray(){
+        List<?>[] namesTable = new List<?>[100];
+        Object[] objects = namesTable;
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        objects[0] = numbers;
+
+        try{
+            String name = (String)namesTable[0].getFirst();
+        } catch (ClassCastException exception){}
+    }
+}
