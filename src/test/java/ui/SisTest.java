6aa0d20eb697d94816d8a0be6903ecdbb0d266a6
iasmin <icb2@ic.ufal.br>
2024-07-31 08:31:24 -0300

    Cria a classe Sis e sua classe de teste.

---
+++ src/test/java/ui/SisTest.java
@@ -0,0 +1,31 @@
+package ui;
+
+import junit.framework.TestCase;
+
+import javax.swing.*;
+
+public class SisTest extends TestCase {
+    private Sis sis;
+    private JFrame frame;
+
+    protected void setUp() {
+        sis = new Sis();
+        frame = sis.getFrame();
+    }
+
+    public void testCreate() {
+        final double tolerance = 0.05;
+        assertEquals(Sis.HEIGHT, frame.getSize().getHeight(), tolerance);
+        assertEquals(Sis.WIDTH, frame.getSize().getWidth(), tolerance);
+        assertEquals(JFrame.EXIT_ON_CLOSE, frame.getDefaultCloseOperation());
+    }
+
+    protected void tearDown() {
+        sis.close();
+    }
+
+    public void testShow() {
+        sis.show();
+        assertTrue(frame.isVisible());
+    }
+}
