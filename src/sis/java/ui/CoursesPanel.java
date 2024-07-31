80ee77bba8c7cd75717927bee307682b2d397163
iasmin <icb2@ic.ufal.br>
2024-07-31 10:34:18 -0300

    Adiciona campos e botão ao painel em CoursesPanel.

--- src/sis/java/ui/CoursesPanel.java
+++ src/sis/java/ui/CoursesPanel.java
@@ -3,16 +3,26 @@ package ui;
 import javax.swing.*;
 
 public class CoursesPanel extends JPanel {
-    static final String LABEL_NAME = "coursesLabel";
     static final String NAME = "cousesPanel";
     static final String LABEL_TEXT = "Courses";
+    static final String LABEL_NAME = "coursesLabel";
+    static final String COURSES_LIST_NAME = "coursesList";
+    static final String ADD_BUTTON_TEXT = "Add";
+    static final String ADD_BUTTON_NAME = "addButton";
+    static final String DEPARTMENT_FIELD_NAME = "deptField";
+    static final String NUMBER_FIELD_NAME = "numberField";
+    static final String DEPARTMENT_LABEL_NAME = "deptLabel";
+    static final String NUMBER_LABEL_NAME = "numberLabel";
+    static final String DEPARTMENT_LABEL_TEXT = "Department";
+    static final String NUMBER_LABEL_TEXT = "Number";
+
     public static void main(String[] args) {
         show(new CoursesPanel());
     }
 
     private static void show(JPanel panel) {
         JFrame frame = new JFrame();
-        frame.setSize(100, 100);
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel);
         frame.setVisible(true);
@@ -26,6 +36,33 @@ public class CoursesPanel extends JPanel {
     private void createLayout() {
         JLabel label = new JLabel(LABEL_TEXT);
         label.setName(LABEL_NAME);
+
+        JList list = new JList();
+        list.setName(COURSES_LIST_NAME);
+
+        JButton addButton = new JButton(ADD_BUTTON_TEXT);
+        addButton.setName(ADD_BUTTON_NAME);
+
+        int columns = 20;
+
+        JLabel departmentLabel = new JLabel(DEPARTMENT_LABEL_TEXT);
+        departmentLabel.setName(DEPARTMENT_LABEL_NAME);
+
+        JTextField departmentField = new JTextField(columns);
+        departmentField.setName(DEPARTMENT_FIELD_NAME);
+
+        JLabel numberLabel = new JLabel(NUMBER_LABEL_TEXT);
+        numberLabel.setName(NUMBER_LABEL_NAME);
+
+        JTextField numberField = new JTextField(columns);
+        numberField.setName(NUMBER_FIELD_NAME);
+
         add(label);
+        add(list);
+        add(addButton);
+        add(departmentLabel);
+        add(departmentField);
+        add(numberLabel);
+        add(numberField);
     }
 }
--- src/test/java/ui/CoursesPanelTest.java
+++ src/test/java/ui/CoursesPanelTest.java
@@ -2,11 +2,30 @@ package ui;
 
 import junit.framework.TestCase;
 import javax.swing.*;
+import static ui.CoursesPanel.*;
 
 public class CoursesPanelTest extends TestCase {
     public void testCreate() {
         CoursesPanel panel = new CoursesPanel();
-        JLabel label = (JLabel) Util.getComponent(panel, CoursesPanel.LABEL_NAME);
-        assertEquals(CoursesPanel.LABEL_TEXT, label.getText());
+        JLabel label = (JLabel) Util.getComponent(panel, LABEL_NAME);
+        assertEquals(LABEL_TEXT, label.getText());
+
+        JList list = (JList) Util.getComponent(panel, COURSES_LIST_NAME);
+        assertEquals(0, list.getModel().getSize());
+
+        JButton button = (JButton) Util.getComponent(panel, ADD_BUTTON_NAME);
+        assertEquals(ADD_BUTTON_TEXT, button.getText());
+
+        JLabel departmentLabel = (JLabel)Util.getComponent(panel, DEPARTMENT_LABEL_NAME);
+        assertEquals(DEPARTMENT_LABEL_TEXT, departmentLabel.getText());
+
+        JTextField departmentField = (JTextField)Util.getComponent(panel, DEPARTMENT_FIELD_NAME);
+        assertEquals("", departmentField.getText());
+
+        JLabel numberLabel = (JLabel) Util.getComponent(panel, NUMBER_LABEL_NAME);
+        assertEquals(NUMBER_LABEL_TEXT, numberLabel.getText());
+
+        JTextField numberField = (JTextField) Util.getComponent(panel, NUMBER_FIELD_NAME);
+        assertEquals("", numberField.getText());
     }
 }
