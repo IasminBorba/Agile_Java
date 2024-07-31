7a7f9c0b3665b1f40e6ad530451f87f2f90da7e6
iasmin <icb2@ic.ufal.br>
2024-07-31 11:12:39 -0300

    Reafatoração.

--- src/sis/java/ui/CoursesPanel.java
+++ src/sis/java/ui/CoursesPanel.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class CoursesPanel extends JPanel {
     static final String NAME = "cousesPanel";
-    static final String LABEL_TEXT = "Courses";
-    static final String LABEL_NAME = "coursesLabel";
+    static final String COURSES_LABEL_TEXT = "Courses";
+    static final String COURSES_LABEL_NAME = "coursesLabel";
     static final String COURSES_LIST_NAME = "coursesList";
     static final String ADD_BUTTON_TEXT = "Add";
     static final String ADD_BUTTON_NAME = "addButton";
@@ -34,35 +34,56 @@ public class CoursesPanel extends JPanel {
     }
 
     private void createLayout() {
-        JLabel label = new JLabel(LABEL_TEXT);
-        label.setName(LABEL_NAME);
+        createLabel(COURSES_LABEL_NAME, COURSES_LABEL_TEXT);
 
-        JList list = new JList();
-        list.setName(COURSES_LIST_NAME);
-
-        JButton addButton = new JButton(ADD_BUTTON_TEXT);
-        addButton.setName(ADD_BUTTON_NAME);
+        createList(COURSES_LIST_NAME);
+        createButton(ADD_BUTTON_NAME, ADD_BUTTON_TEXT);
 
         int columns = 20;
+        createLabel(DEPARTMENT_LABEL_NAME, DEPARTMENT_LABEL_TEXT);
+        createField(DEPARTMENT_FIELD_NAME, columns);
 
-        JLabel departmentLabel = new JLabel(DEPARTMENT_LABEL_TEXT);
-        departmentLabel.setName(DEPARTMENT_LABEL_NAME);
+        createLabel(NUMBER_LABEL_NAME, NUMBER_LABEL_TEXT);
+        createField(NUMBER_FIELD_NAME, columns);
+    }
 
-        JTextField departmentField = new JTextField(columns);
-        departmentField.setName(DEPARTMENT_FIELD_NAME);
-
-        JLabel numberLabel = new JLabel(NUMBER_LABEL_TEXT);
-        numberLabel.setName(NUMBER_LABEL_NAME);
-
-        JTextField numberField = new JTextField(columns);
-        numberField.setName(NUMBER_FIELD_NAME);
-
+    private void createLabel(String name, String text) {
+        JLabel label = new JLabel(text);
+        label.setName(name);
         add(label);
+    }
+
+    private void createList(String name) {
+        JList list = new JList();
+        list.setName(name);
         add(list);
-        add(addButton);
-        add(departmentLabel);
-        add(departmentField);
-        add(numberLabel);
-        add(numberField);
+    }
+
+    private void createButton(String name, String text) {
+        JButton button = new JButton(text);
+        button.setName(name);
+        add(button);
+    }
+
+    private void createField(String name, int columns) {
+        JTextField field = new JTextField(columns);
+        field.setName(name);
+        add(field);
+    }
+
+    JLabel getLabel(String name) {
+        return (JLabel) Util.getComponent(this, name);
+    }
+
+    JList getList(String name) {
+        return (JList) Util.getComponent(this, name);
+    }
+
+    JButton getButton(String name) {
+        return (JButton) Util.getComponent(this, name);
+    }
+
+    JTextField getField(String name) {
+        return (JTextField) Util.getComponent(this, name);
     }
 }
