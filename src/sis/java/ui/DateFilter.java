9003b3b43dce0b0c538ad4e4f01af8249c90110f
iasmin <icb2@ic.ufal.br>
2024-08-09 15:35:51 -0300

    Adiciona filtro pra data.

---
+++ src/sis/java/ui/DateFilter.java
@@ -0,0 +1,60 @@
+package ui;
+
+import javax.swing.text.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateFilter extends ChainableFilter {
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yy");
+    private static final DateTimeFormatter DISPLAY_FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+    @Override
+    public void insertString(FilterBypass bypass, int offset, String text, AttributeSet attr) throws BadLocationException {
+        String newText = getTextFromDocument(bypass).substring(0, offset) + text + getTextFromDocument(bypass).substring(offset);
+        try {
+            LocalDate date = LocalDate.parse(newText, INPUT_FORMATTER);
+            String formattedDate = date.format(DISPLAY_FORMATTER);
+            applyNextInsert(bypass, offset, formattedDate, attr);
+        } catch (DateTimeParseException e) {
+            applyNextInsert(bypass, offset, "", attr);
+        }
+    }
+
+    @Override
+    public void replace(FilterBypass bypass, int offset, int length, String text, AttributeSet attr) throws BadLocationException {
+        String doc = getTextFromDocument(bypass);
+        String newText = getTextFromDocument(bypass).substring(0, offset) + text + getTextFromDocument(bypass).substring(offset + length);
+
+        try {
+            LocalDate date = LocalDate.parse(newText, INPUT_FORMATTER);
+            String formattedDate = date.format(DISPLAY_FORMATTER);
+            applyNextReplace(bypass, 0, doc.length(), formattedDate, attr);
+        } catch (DateTimeParseException e) {
+            if (text.matches("\\d*")) {
+                applyNextReplace(bypass, offset, length, text, attr);
+
+                if (newText.length() == 2) {
+                    applyNextReplace(bypass, offset + 1, length, "/", attr);
+                }
+
+                if (Integer.parseInt(newText.substring(0, 2)) > 12)
+                    applyNextReplace(bypass, 0, 3, "12/", attr);
+
+                if (newText.length() == 5) {
+                    applyNextReplace(bypass, offset + 1, length, "/", attr);
+                }
+
+                if (Integer.parseInt(newText.substring(3, 5)) > 31) {
+                    applyNextReplace(bypass, 3, 3, "30/", attr);
+                }
+            } else {
+                applyNextInsert(bypass, offset, "", attr);
+            }
+        }
+    }
+
+    private String getTextFromDocument(FilterBypass bypass) throws BadLocationException {
+        return bypass.getDocument().getText(0, bypass.getDocument().getLength());
+    }
+}
---
+++ src/test/java/ui/DateFilterTest.java
@@ -0,0 +1,95 @@
+package ui;
+
+import junit.framework.TestCase;
+
+import javax.swing.text.*;
+
+public class DateFilterTest extends TestCase {
+    private DocumentFilter filter;
+    protected DocumentFilter.FilterBypass bypass;
+    protected AbstractDocument document;
+    String date = "01/01/24";
+
+    protected void setUp() {
+        bypass = createBypass();
+        document = (AbstractDocument)bypass.getDocument();
+        filter = new DateFilter();
+    }
+
+    public void testInsert() throws BadLocationException {
+        filter.insertString(bypass, 0, date, null);
+        assertEquals("01/01/2024", documentText());
+
+        filter.insertString(bypass, 1, "05/05/24", null);
+        assertEquals("01/01/2024", documentText());
+
+        setUp();
+        filter.insertString(bypass, 0, "def", null);
+        assertEquals("", documentText());
+
+        filter.insertString(bypass, 0, "010505", null);
+        assertEquals("", documentText());
+
+        filter.insertString(bypass, 0, "21/12/24", null);
+        assertEquals("", documentText());
+
+        filter.insertString(bypass, 0, "09/08/", null);
+        filter.insertString(bypass, 0, "24", null);
+        assertEquals("", documentText());
+    }
+
+    public void testReplace() throws BadLocationException {
+        filter.insertString(bypass, 0, "09/08/24", null);
+        filter.replace(bypass,0,2,"tc", null);
+        assertEquals("09/08/2024", documentText());
+
+        filter.replace(bypass, 0, 2, "30", null);
+        assertEquals("12/08/2024", documentText());
+
+        filter.replace(bypass, 0, 2, "12", null);
+        assertEquals("12/08/2024", documentText());
+
+        filter.replace(bypass, 3, 2, "32", null);
+        assertEquals("12/30/2024", documentText());
+
+        filter.replace(bypass, 8, 2, "25", null);
+        assertEquals("12/30/2025", documentText());
+
+        filter.replace(bypass, 6, 2, "10", null);
+        assertEquals("12/30/1025", documentText());
+
+        filter.replace(bypass, 2, 1, "A", null);
+        assertEquals("12/30/1025", documentText());
+
+        filter.replace(bypass, 0, 1, "?", null);
+        assertEquals("12/30/1025", documentText());
+    }
+
+    protected String documentText() throws BadLocationException {
+        return document.getText(0, document.getLength());
+    }
+
+    protected DocumentFilter.FilterBypass createBypass() {
+        return new DocumentFilter.FilterBypass() {
+            private final AbstractDocument document = new PlainDocument();
+
+            public Document getDocument() {
+                return document;
+            }
+
+            public void insertString(int offset, String string, AttributeSet attr) {
+                try {
+                    document.insertString(offset, string, attr);
+                } catch (BadLocationException e) {}
+            }
+
+            public void remove(int offset, int length) throws BadLocationException {}
+
+            public void replace(int offset, int length, String string, AttributeSet attrs) throws BadLocationException {
+                try {
+                    document.replace(offset, length, string, attrs);
+                } catch (BadLocationException e) {}
+            }
+        };
+    }
+}
