b10587c3a5b2959394033a839fa3f478d19c31c6
iasmin <icb2@ic.ufal.br>
2024-09-03 11:08:43 -0300

    Refatorando a classe Clock.

--- src/sis/java/clock/Clock.java
+++ src/sis/java/clock/Clock.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public class Clock implements Runnable {
     private final ClockListener listener;
     private boolean run = true;
+    long lastTime;
 
     public Clock(ClockListener listener) {
         this.listener = listener;
@@ -17,17 +18,24 @@ public class Clock implements Runnable {
 
     public void run() {
         Thread.currentThread().setPriority(Thread.NORM_PRIORITY - 1);
-        long lastTime = System.currentTimeMillis();
+        lastTime = System.currentTimeMillis();
         while (run) {
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
             }
-            long now = System.currentTimeMillis();
-            if ((now / 1000) - (lastTime / 1000) >= 1) {
-                listener.update(LocalDateTime.now());
-                lastTime = now;
-            }
+            checkSeconds();
         }
+    }
+
+    private void checkSeconds() {
+        long now = System.currentTimeMillis();
+        if((now / 1000) - (lastTime / 1000) >= 1)
+            updateTime(now);
+    }
+
+    private void updateTime(long now) {
+        listener.update(LocalDateTime.now());
+        lastTime = now;
     }
 }
